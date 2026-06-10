f77c0b7fd6807b0b397f61ce1b36e056df5e1c94
sean <seanozalpasan@gmail.com>
2026-06-09 21:34:48 -0700

    setup trust zone on the MCU. blinking an LED works

---
+++ NonSecure/Debug/objects.mk
@@ -0,0 +1,9 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (14.3.rel1)
+################################################################################
+
+USER_OBJS := C:/Users/sean/seanprojects/trustzoneBlinkLED/Secure/Debug/secure_nsclib.o
+
+LIBS :=
+
